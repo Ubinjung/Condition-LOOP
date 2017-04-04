451fd46bd1dd39c0e401373f32bf037cfa82d850
Ubin Jung <ubin771200@gmail.com>
2017-04-04 22:01:56 +0900

    Add files via upload

---
+++ fors.java
@@ -0,0 +1,15 @@
+
+public class fors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		int total;
+		for(i=1,total=0;i <= 5;i++){
+			
+			total += i;
+			System.out.printf("1에서 %d까지 합은 %d입니다", total);
+		}
+	}
+
+}
